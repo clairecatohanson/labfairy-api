46209ba5240a7feed36625f0c1edd41555a38a86
Claire Hanson <clairecatohanson@gmail.com>
2024-04-26 15:13:18 -0500

    updated Room model field number to name

--- labfairyapi/models/room.py
+++ labfairyapi/models/room.py
@@ -5,7 +5,7 @@ class Room(models.Model):
     building = models.ForeignKey(
         "Building", on_delete=models.CASCADE, related_name="rooms"
     )
-    number = models.IntegerField()
+    name = models.CharField(max_length=10)
 
     def __str__(self):
-        return f"{self.building.name} - Room {self.number}"
+        return f"{self.building.name} - Room {self.name}"
